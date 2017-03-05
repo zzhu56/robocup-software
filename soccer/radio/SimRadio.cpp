8867eb45a9525dd79150e26d4b948af7816fd1e0
Albert Shaw <ashaw596@gmail.com>
2017-03-05 01:00:48 -0500

    Update protos so that theres no nano callbacks

--- soccer/radio/SimRadio.cpp
+++ soccer/radio/SimRadio.cpp
@@ -6,6 +6,10 @@
 #include "firmware-common/common2015/utils/rtp.hpp"
 #include "google/protobuf/io/zero_copy_stream_impl_lite.h"
 #include "google/protobuf/io/gzip_stream.h"
+#include "pb_decode.h"
+#include "pb_encode.h"
+#include "protobuf/nanopb/RadioTx.pb.h"
+#include <array>
 using namespace std;
 using namespace Packet;
 
@@ -30,6 +34,25 @@ void SimRadio::send(Packet::RobotsTxPacket& packet) {
     //google::protobuf::io::StringOutputStream stream(&out);
     //google::protobuf::io::GzipOutputStream gzip(&stream);
     packet.SerializeToString(&out);
+
+    /*
+    Packet_RobotsTxPacket p = Packet_RobotsTxPacket_init_zero;
+    pb_istream_t stream = pb_istream_from_buffer(reinterpret_cast<const unsigned char *>( &out[0]), out.length()+1);
+    auto status = pb_decode(&stream, Packet_RobotsTxPacket_fields, &p);
+    cout<<status<<endl;
+
+    std::string buffer;
+    buffer.resize(Packet_RobotsTxPacket_size);
+    auto oStream = pb_ostream_from_buffer(reinterpret_cast<unsigned char *>(&buffer[0]), buffer.size());
+    status = pb_encode(&oStream, Packet_RobotsTxPacket_fields, &p);
+    cout<<status<<endl;
+    cout<<oStream.bytes_written<<endl;
+
+    Packet::RobotsTxPacket asdf;
+    asdf.ParseFromString(buffer);
+    cout<<(asdf.SerializeAsString() == packet.SerializeAsString())<<endl;
+
+     */
     //cout<<gzip.ByteCount()<<endl;
     //gzip.Close();
     //cout<<stream.ByteCount()<<endl;
